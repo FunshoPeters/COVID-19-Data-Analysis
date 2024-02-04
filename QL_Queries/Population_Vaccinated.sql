e234f69de3b6f6ede8043c22679e7aae447dfcb0
Funsho Peters <69419175+FunshoPeters@users.noreply.github.com>
2024-02-04 19:27:20 +0000

    Create Population_Vaccinated.sql

---
+++ QL_Queries/Population_Vaccinated.sql
@@ -0,0 +1,13 @@
+/*
+POPULATION VS VACCINATION ANALYSIS
+Tracks vaccination progress by calculating the rolling sum of new vaccinations, aiming to determine the portion of the population vaccinated over time.
+*/
+
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+    On dea.location = vac.location
+    and dea.date = vac.date
+where dea.continent is not null 
+order by 2,3
