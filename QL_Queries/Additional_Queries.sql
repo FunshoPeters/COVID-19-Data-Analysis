4d54556f0500eed90b25bbd18ebd73dbfec13801
Funsho Peters <69419175+FunshoPeters@users.noreply.github.com>
2024-02-04 20:00:35 +0000

    Create Additional_Queries.sql

---
+++ QL_Queries/Additional_Queries.sql
@@ -0,0 +1,50 @@
+-- CTE FOR VACCINATION PERCENTAGE CALCULATION
+-- Utilizes a Common Table Expression (CTE) to calculate the percentage of the population vaccinated, enhancing readability and analysis flexibility.
+With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
+as
+(
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+    On dea.location = vac.location
+    and dea.date = vac.date
+where dea.continent is not null 
+)
+Select *, (RollingPeopleVaccinated/Population)*100
+From PopvsVac
+
+-- TEMP TABLE FOR VACCINATION ANALYSIS
+-- Creates a temporary table to facilitate complex calculations on vaccination data, offering a detailed look at the percentage of the population vaccinated over time.
+DROP Table if exists #PercentPopulationVaccinated
+Create Table #PercentPopulationVaccinated
+(
+Continent nvarchar(255),
+Location nvarchar(255),
+Date datetime,
+Population numeric,
+New_vaccinations numeric,
+RollingPeopleVaccinated numeric
+)
+
+Insert into #PercentPopulationVaccinated
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+    On dea.location = vac.location
+    and dea.date = vac.date
+
+Select *, (RollingPeopleVaccinated/Population)*100
+From #PercentPopulationVaccinated
+
+-- VIEW FOR VACCINATION DATA
+-- Establishes a view to store and easily access calculated vaccination data, supporting future analysis and visualization efforts.
+Create View PercentPopulationVaccinated as
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+    On dea.location = vac.location
+    and dea.date = vac.date
+where dea.continent is not null 
